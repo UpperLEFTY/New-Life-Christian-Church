b2b8047ceec5c03bf582779d086879e3b24c2612
UpperLEFTY <upperlefty@zoho.eu>
2017-12-23 23:45:32 +0200

    imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './mixins.scss';
-import './navbar.scss';
 import './normalize.scss';
 import './variables.scss';
 import './style.scss';
