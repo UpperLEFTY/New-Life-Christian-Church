3e4de798f37bc11c0cb61273477d667539e6ec79
UpperLEFTY <upperlefty@zoho.eu>
2017-12-23 23:44:56 +0200

    imports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import './mixins.scss';
 import './navbar.scss';
-import './navs.scss';
 import './normalize.scss';
 import './variables.scss';
 import './style.scss';
