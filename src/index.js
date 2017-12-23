3cafadc3f09fa4a5c979d442661bfe8d77fad657
UpperLEFTY <upperlefty@zoho.eu>
2017-12-23 23:47:33 +0200

    imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './mixins.scss';
-import './normalize.scss';
 import './variables.scss';
 import './style.scss';
 import registerServiceWorker from './registerServiceWorker';
